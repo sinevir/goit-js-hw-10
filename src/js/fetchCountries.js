12b3fce5f5025588a8ec0e5ca736227abbfcc83b
Oleh Stashchyshyn <o.s.dubbie@gmail.com>
2023-04-09 21:39:24 +0300

    homework 10 ready

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,3 @@
-// const BASE_URL = 'https://restcountries.com/v3.1/name';
-
 export const fetchCountries = name => {
     return fetch(
       `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
